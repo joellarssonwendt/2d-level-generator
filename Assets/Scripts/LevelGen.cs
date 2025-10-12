c9109b8aefb355d03295a60df8f57e1c710686bc
Joel Larsson Wendt <156666927+joellarssonwendt@users.noreply.github.com>
2025-10-13 01:05:45 +0200

    LevelGen: HashSeed and PerlinNoise sampling

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -1,4 +1,3 @@
-using NUnit.Framework.Internal;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -18,27 +17,95 @@ public class LevelGen : MonoBehaviour
         }
     }
 
+    [SerializeField] private string seedString = "";
     [SerializeField] private Tilemap tilemap;
     [SerializeField] private TileBase defaultTile;
     [SerializeField] private int width, height;
+
     private int[,] grid;
+    private System.Random rng;
 
     void Start()
     {
-        grid = new int[width, height];
-        Test();
+        HashSeed();
+        GenerateGrid();
+        DrawTiles();
     }
 
-    private void Test()
+    private void HashSeed()
     {
+        int seed;
+
+        if (string.IsNullOrEmpty(seedString))
+        {
+            seed = Random.Range(1, int.MaxValue);
+
+            Debug.Log($"World seed: {seed}");
+        }
+        else
+        {
+            seed = 23;
+
+            unchecked // Allow overflow
+            {
+                foreach (char c in seedString)
+                {
+                    seed = seed * 31 + c;
+                }
+            }
+
+            if (seed < 0)
+            {
+                seed = seed & 0x7FFFFFFF; // Remove sign bit
+            }
+
+            Debug.Log($"World seed: {seedString}");
+        }
+
+        Debug.Log($"Hidden seed: {seed}");
+        rng = new System.Random(seed);
+    }
+
+    private void GenerateGrid()
+    {
+        grid = new int[width, height];
+
+        float f = (float)rng.NextDouble();
+
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                if (y == 0)
+                float noise = Mathf.PerlinNoise(x * f, y * f);
+
+                if (noise > 0.5f)
                 {
                     grid[x, y] = 1;
-                    tilemap.SetTile(new Vector3Int(x, y, 0), defaultTile);
+                }
+            }
+        }
+    }
+
+    private void DrawTiles()
+    {
+        Vector3Int pos = new();
+
+        for (int y = 0; y < height; y++)
+        {
+            pos.y = y;
+
+            for (int x = 0; x < width; x++)
+            {
+                pos.x = x;
+
+                if (grid[x, y] == 1)
+                {
+                    TileBase tile = defaultTile;
+                    tilemap.SetTile(pos, tile);
+                }
+                else
+                {
+                    tilemap.SetTile(pos, null);
                 }
             }
         }

13a5d6ace8f71a1806edc81e510cd4e7c402ce6c
Joel Larsson Wendt <156666927+joellarssonwendt@users.noreply.github.com>
2025-11-05 10:13:57 +0100

    Inverted Perlin sampling, + new editor version

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -80,11 +80,13 @@ public class LevelGen : MonoBehaviour
         {
             for (int x = 0; x < width; x++)
             {
+                grid[x, y] = 1;
+
                 float noise = Mathf.PerlinNoise(x * granularity, y * granularity);
 
-                if (noise > 0.5f)
+                if (noise > 0.4f)
                 {
-                    grid[x, y] = 1;
+                    grid[x, y] = 0;
                 }
             }
         }
